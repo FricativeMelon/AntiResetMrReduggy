0c6b47ede36bb2dd0eff57a2d72257395efb8c28
FricativeMelon <43120800+FricativeMelon@users.noreply.github.com>
2023-02-22 21:17:30 -0500

    Removed NPC data from Mr. Reduggy

--- AntiResetMrReduggy/ModEntry.cs
+++ AntiResetMrReduggy/ModEntry.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using Microsoft.Win32.SafeHandles;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Netcode;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -37,11 +39,19 @@ namespace AntiResetMrReduggy
     class ReduggyDialogueBox : DialogueBox
     {
         public string oldDialogueString;
+        private Texture2D te;
 
-        public ReduggyDialogueBox(string dialogue, string preface = "")
-            : base(new Dialogue(preface+dialogue, Game1.getCharacterFromName("Mr. Reduggy", false)))
+        public ReduggyDialogueBox(Texture2D te, string dialogue, string preface = "")
+            : base(new Dialogue(preface+dialogue, MakeReduggy(te)))
         {
+            this.te = te;
             oldDialogueString = dialogue;
+        }
+
+
+        private static NPC MakeReduggy(Texture2D te)
+        {
+            return new NPC(null, new Vector2(0, 0), "Mines", 0, "Mr. Reduggy", false, null, te);
         }
 
         public override void receiveRightClick(int x, int y, bool playSound = true)
@@ -53,7 +63,7 @@ namespace AntiResetMrReduggy
         {
             if (characterIndexInDialogue < getCurrentString().Length - 1)
             {
-                Game1.activeClickableMenu =  new ReduggyDialogueBox(oldDialogueString, "Seriously? You're not even listening, are you?"
+                Game1.activeClickableMenu =  new ReduggyDialogueBox(te, oldDialogueString, "Seriously? You're not even listening, are you?"
                     + " I'd better start from the top, since you obviously haven't heard a word I've said.$5#$b#");
             }
             else
@@ -79,6 +89,8 @@ namespace AntiResetMrReduggy
 
         private int time = 0;
 
+        private Texture2D reduggyPortraits;
+
         /*********
         ** Public methods
         *********/
@@ -90,9 +102,22 @@ namespace AntiResetMrReduggy
             helper.Events.Player.Warped += this.OnWarp;
             helper.Events.GameLoop.DayStarted += this.OnDayStarted;
             helper.Events.GameLoop.TimeChanged += this.OnTimeChanged;
+            helper.Events.Content.AssetRequested += this.OnAssetRequested;
+
+            reduggyPortraits = Helper.ModContent.Load<Texture2D>("assets/portraits.png");
         }
 
-        private string GetReduggyString(ModData context)
+        private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
+        {
+            if (e.Name.IsEquivalentTo("FricativeMelon.AntiResetMrReduggy"))
+            {
+                e.LoadFromModFile
+                    <Texture2D>
+                    (Path.Combine("assets", "portraits.png") , AssetLoadPriority.Exclusive);
+            }
+        }
+
+            private string GetReduggyString(ModData context)
         {
             IDictionary<string, string> data = Helper.ModContent.Load<Dictionary<string, string>>("assets/data.json");
             string res = data["intro"+Math.Min(6, context.runs-1).ToString()];
@@ -201,7 +226,7 @@ namespace AntiResetMrReduggy
                     /*Game1.player.setTileLocation(new Vector2(64, 15));
                     Game1.player.faceDirection(2);
                     Game1.player.position.Y -= 16f;*/
-                    DialogueBox db = new ReduggyDialogueBox(GetReduggyString(model));
+                    DialogueBox db = new ReduggyDialogueBox(reduggyPortraits, GetReduggyString(model));
                     Game1.activeClickableMenu = db;
                 }
             }
